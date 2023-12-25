73d84a8500e5d90434817ce6e8f054c21014039f
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 23:55:11 +0200

    add inputTraining

---
+++ src/main/java/com/edhealthbackend/GqlModel/inputTraining.java
@@ -0,0 +1,13 @@
+package com.edhealthbackend.GqlModel;
+
+import com.edhealthbackend.Interfaces.graphQlData;
+import com.edhealthbackend.Model.Training;
+
+public class inputTraining extends Training implements graphQlData<Training> {
+
+    @Override
+    public Training getData() {
+       return new Training(getId(), getDescription(), getPostDate(), getDeadline());
+    }
+
+}
