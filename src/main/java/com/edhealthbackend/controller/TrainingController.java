7c6e41cb35f7b56c8c1748c8d0b6c80930976570
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 23:50:42 +0200

    add findTrainingById method

--- src/main/java/com/edhealthbackend/controller/TrainingController.java
+++ src/main/java/com/edhealthbackend/controller/TrainingController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+// import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import com.edhealthbackend.GqlModel.inputTraining;
 import com.edhealthbackend.Model.Training;
 import com.edhealthbackend.Repository.TrainingRepo;
 import lombok.extern.slf4j.Slf4j;
@@ -15,25 +18,16 @@ import lombok.extern.slf4j.Slf4j;
 public class TrainingController {
 @Autowired private TrainingRepo ac_ResultRepo;
 @MutationMapping()
-public Training saveAccademicResult(@Argument(name = "input")Training data){
-    return ac_ResultRepo.save(data);
+public Training saveTraining(@Argument(name = "input")inputTraining data){
+    return ac_ResultRepo.save(data.getData());
 }
+
 @MutationMapping()
-public Training findById(@Argument(name = "input")long data){
+public String deleteTraining(@Argument long id){
     try {
-         return ac_ResultRepo.findById(data).orElse(null);
-    } catch (Exception e) {
-        log.info(e.getMessage());
-        return null;
-    }
-   
-}
-@MutationMapping()
-public String deleteAccademicResult(@Argument()long data){
-    try {
-        Training Training=ac_ResultRepo.findById(data).orElse(null);
+        Training Training=ac_ResultRepo.findById(id).orElse(null);
       if(Training!=null) {
-        ac_ResultRepo.deleteById(data); 
+        ac_ResultRepo.deleteById(id); 
         return "Accademic Deleted Sucessfully";
     } 
     else return "Please Select Academic record";
@@ -42,8 +36,14 @@ public String deleteAccademicResult(@Argument()long data){
     }
 }
 @QueryMapping()
-public Training findTrainingById(@Argument() long data){
-return ac_ResultRepo.findById(data).orElse(null);
+public Training findTrainingById(@Argument long id){
+    try {
+         return ac_ResultRepo.findById(id).orElse(null);
+    } catch (Exception e) {
+        log.info(e.getMessage());
+        return null;
+    }
+   
 }
 @QueryMapping()
 public List<Training> getAllTrainings(){
