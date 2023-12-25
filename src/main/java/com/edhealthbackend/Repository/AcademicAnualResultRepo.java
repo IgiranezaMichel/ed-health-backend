bea5a35d93e3be63e7aae11a434ffa84832550d5
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 10:39:52 +0200

    Add AcademicAnualResultRepo

---
+++ src/main/java/com/edhealthbackend/Repository/AcademicAnualResultRepo.java
@@ -0,0 +1,10 @@
+package com.edhealthbackend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edhealthbackend.Model.AcademicAnualResult;
+@Repository
+public interface AcademicAnualResultRepo extends JpaRepository<AcademicAnualResult,Long>{
+
+}
