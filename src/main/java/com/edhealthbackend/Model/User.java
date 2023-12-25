63b8d0aa9adc1cbef70ff2803b266c26457b55b0
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 15:11:02 +0200

    feed Attribute to User

--- src/main/java/com/edhealthbackend/Model/User.java
+++ src/main/java/com/edhealthbackend/Model/User.java
@@ -1,5 +1,7 @@
 package com.edhealthbackend.Model;
 
+import java.util.Date;
+
 import jakarta.persistence.*;
 import lombok.*;
 @Entity
@@ -9,4 +11,12 @@ import lombok.*;
 public class User {
 @Id @GeneratedValue(strategy = GenerationType.AUTO)
 private long id;
+private String name;
+private String gender;
+private String profilePicture;
+private String phoneNumber;
+private String email;
+private Date dob;
+private String role;
+private String password;
 }
