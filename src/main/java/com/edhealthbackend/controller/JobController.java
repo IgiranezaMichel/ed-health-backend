47e14a8778acd29ad595c11cd3a88fbd9c4dbe97
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 23:49:30 +0200

    add findJobById method

--- src/main/java/com/edhealthbackend/controller/JobController.java
+++ src/main/java/com/edhealthbackend/controller/JobController.java
@@ -8,6 +8,7 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.edhealthbackend.GqlModel.inputJob;
 import com.edhealthbackend.Model.Job;
 import com.edhealthbackend.Repository.JobRepo;
 
@@ -18,35 +19,32 @@ import lombok.extern.slf4j.Slf4j;
 public class JobController {
 @Autowired private JobRepo ac_ResultRepo;
 @MutationMapping()
-public Job saveAccademicResult(@Argument(name = "input")Job data){
-    return ac_ResultRepo.save(data);
+public Job saveJob(@Argument(name = "input")inputJob data){
+    return ac_ResultRepo.save(data.getData());
 }
+
 @MutationMapping()
-public Job findById(@Argument(name = "input")long data){
+public String deleteJob(@Argument long id){
     try {
-         return ac_ResultRepo.findById(data).orElse(null);
-    } catch (Exception e) {
-        log.info(e.getMessage());
-        return null;
-    }
-   
-}
-@MutationMapping()
-public String deleteJob(@Argument()long data){
-    try {
-        Job Job=ac_ResultRepo.findById(data).orElse(null);
+        Job Job=ac_ResultRepo.findById(id).orElse(null);
       if(Job!=null) {
-        ac_ResultRepo.deleteById(data); 
-        return "Accademic Deleted Sucessfully";
+        ac_ResultRepo.deleteById(id); 
+        return "Job Deleted Sucessfully";
     } 
     else return "Please Select Academic record";
     } catch (Exception e) {
         return "Error haappen";
     }
 }
-@QueryMapping()
-public Job findJobById(@Argument() long data){
-return ac_ResultRepo.findById(data).orElse(null);
+@QueryMapping
+public Job findJobById(@Argument long id){
+    try {
+         return ac_ResultRepo.findById(id).orElse(null);
+    } catch (Exception e) {
+        log.info(e.getMessage());
+        return null;
+    }
+   
 }
 @QueryMapping()
 public List<Job> getAllJobs(){
