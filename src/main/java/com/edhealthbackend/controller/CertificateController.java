bc785319d982437aa0b6bf8b7a2354894abd2517
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 23:48:38 +0200

    add findCertificateById

--- src/main/java/com/edhealthbackend/controller/CertificateController.java
+++ src/main/java/com/edhealthbackend/controller/CertificateController.java
@@ -8,6 +8,7 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.edhealthbackend.GqlModel.inputCertificate;
 import com.edhealthbackend.Model.Certificate;
 import com.edhealthbackend.Repository.CertificateRepo;
 import lombok.extern.slf4j.Slf4j;
@@ -16,26 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 public class CertificateController {
 @Autowired private CertificateRepo certificateRepo;
 @MutationMapping()
-public Certificate saveCertificate(@Argument(name = "input")Certificate data){
-    return certificateRepo.save(data);
+public Certificate saveCertificate(@Argument(name = "input")inputCertificate data){
+    return certificateRepo.save(data.getData());
 }
 @MutationMapping()
-public Certificate findById(@Argument(name = "input")long data){
+public String deleteCertificate(@Argument()long id){
     try {
-         return certificateRepo.findById(data).orElse(null);
-    } catch (Exception e) {
-        log.info(e.getMessage());
-        return null;
-    }
-   
-}
-@MutationMapping()
-public String deleteCertificate(@Argument()long data){
-    try {
-        Certificate Certificate=certificateRepo.findById(data).orElse(null);
+        Certificate Certificate=certificateRepo.findById(id).orElse(null);
       if(Certificate!=null) {
-        certificateRepo.deleteById(data); 
-        return "Accademic Deleted Sucessfully";
+        certificateRepo.deleteById(id); 
+        return "Certificate Deleted Sucessfully";
     } 
     else return "Please Select Academic record";
     } catch (Exception e) {
@@ -43,11 +34,16 @@ public String deleteCertificate(@Argument()long data){
     }
 }
 @QueryMapping()
-public Certificate findCertificateById(@Argument() long data){
-return certificateRepo.findById(data).orElse(null);
+public Certificate findCertificateById(@Argument long id){
+    try {
+         return certificateRepo.findById(id).orElse(null);
+    } catch (Exception e) {
+        log.info(e.getMessage());
+        return null;
+    }
 }
 @QueryMapping()
-public List<Certificate> getAllCertificates(){
+public List<Certificate> getAllCertificate(){
     return certificateRepo.findAll();
 }
 }
