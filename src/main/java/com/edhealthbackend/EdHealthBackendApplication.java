e64c666945797958b64869e83b4faa5f68c2ade8
IgiranezaMichel <igiramichael@gmail.com>
2023-12-25 09:31:52 +0200

    first commit

---
+++ src/main/java/com/edhealthbackend/EdHealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edhealthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdHealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdHealthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edhealthbackend/EdHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edhealthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdHealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
