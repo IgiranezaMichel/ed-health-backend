d6779e2e0fac95609f8c6e9431aba92f1a795083
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:45:56 +0200

    add different methods to find

--- src/main/java/com/edhealthbackend/Repository/TrainingRepo.java
+++ src/main/java/com/edhealthbackend/Repository/TrainingRepo.java
@@ -1,9 +1,13 @@
 package com.edhealthbackend.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edhealthbackend.Model.Training;
 
 public interface TrainingRepo extends JpaRepository<Training,Long>{
+    Page<Training> findAllByisApprovedByCnm(PageRequest of, Boolean status);
+    Page<Training> findAllByisDeniedByCnm(PageRequest of, Boolean status);
 
 }
