14b8478b1c443308af78e0620cccd2c991666b04
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:36:57 +0200

    add school constructor

--- src/main/java/com/edhealthbackend/Model/School.java
+++ src/main/java/com/edhealthbackend/Model/School.java
@@ -1,4 +1,6 @@
 package com.edhealthbackend.Model;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 import lombok.*;
 @Entity
@@ -9,4 +11,16 @@ public class School {
 @Id @GeneratedValue(strategy = GenerationType.AUTO)
 private long id;
 private String name;
+@Column(columnDefinition="longBlob")
+private String logo;
+@ManyToOne()
+private Location location;
+private LocalDateTime createdOn;
+public School(long id, String name,String logo,LocalDateTime createdOn,Location location){
+    this.id=id;
+    this.name=name;
+    this.logo=logo;
+    this.createdOn=LocalDateTime.now();
+    this.location=location;
+}
 }
