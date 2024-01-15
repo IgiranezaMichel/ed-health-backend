cac9e4b0023ef689f1adf09537eea700f4841632
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:30:04 +0200

    remove graphQlData interface

--- src/main/java/com/edhealthbackend/GqlModel/inputSchool.java
+++ src/main/java/com/edhealthbackend/GqlModel/inputSchool.java
@@ -1,13 +1,10 @@
 package com.edhealthbackend.GqlModel;
 
-import com.edhealthbackend.Interfaces.graphQlData;
 import com.edhealthbackend.Model.School;
 
-public class inputSchool extends School implements graphQlData<School>{
-
-    @Override
-    public School getData() {
-        return new School(getId(), getName());
-    }
-
+import lombok.Getter;
+import lombok.Setter;
+public class inputSchool extends School{
+    @Getter @Setter
+    private long locationId;
 }
