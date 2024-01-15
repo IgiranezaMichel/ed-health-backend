44f83be0d27c19d3fa8b4388f6761e749f773e81
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:35:48 +0200

    add hospital Logo,location and trainingList

--- src/main/java/com/edhealthbackend/Model/Hospital.java
+++ src/main/java/com/edhealthbackend/Model/Hospital.java
@@ -1,5 +1,8 @@
 package com.edhealthbackend.Model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.*;
 @Entity
@@ -10,4 +13,14 @@ public class Hospital {
 @Id @GeneratedValue(strategy = GenerationType.AUTO)
 private long id;
 private String  name;
+@Column(columnDefinition = "longBlob")
+private String logo;
+private LocalDateTime createdOn=LocalDateTime.now();
+@ManyToOne
+private Location location;
+@OneToMany(mappedBy="hospital")
+private List<Training>trainingList;
+public Hospital(long id,String  name,String logo,LocalDateTime createdOn,Location location){
+this(id, name, logo, createdOn, location, null);
+}
 }
