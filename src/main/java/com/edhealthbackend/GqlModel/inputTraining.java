588dc3ed181ce76e9118b1b696d2a81d4e3ee514
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:30:27 +0200

    remove graphQlData interface

--- src/main/java/com/edhealthbackend/GqlModel/inputTraining.java
+++ src/main/java/com/edhealthbackend/GqlModel/inputTraining.java
@@ -1,13 +1,9 @@
 package com.edhealthbackend.GqlModel;
-
-import com.edhealthbackend.Interfaces.graphQlData;
 import com.edhealthbackend.Model.Training;
 
-public class inputTraining extends Training implements graphQlData<Training> {
+import lombok.*;
 
-    @Override
-    public Training getData() {
-       return new Training(getId(), getTitle(), getDescription(), getPostDate(), getDeadline());
-    }
-
+public class inputTraining extends Training{
+@Getter @Setter
+private long hospitalId;
 }
--- src/main/java/com/edhealthbackend/GqlModel/inputUser.java
+++ src/main/java/com/edhealthbackend/GqlModel/inputUser.java
@@ -5,10 +5,6 @@ import org.mindrot.jbcrypt.BCrypt;
 import com.edhealthbackend.Interfaces.graphQlData;
 import com.edhealthbackend.Model.User;
 
-public class inputUser extends User implements  graphQlData<User>{
-    @Override
-    public User getData() {
-       return new User(getId(), getName(), getGender(), getProfilePicture(), getPhoneNumber(), getEmail(), getDob(), getRole(),BCrypt.hashpw(getPassword(),BCrypt.gensalt()));
-    }
-
+public class inputUser extends User{
+    
 }
