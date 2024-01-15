59d8f00ca6a076a1dcbb8807c038be92d0449b0c
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:38:58 +0200

    add findAllByTypr

--- src/main/java/com/edhealthbackend/Repository/LocationRepo.java
+++ src/main/java/com/edhealthbackend/Repository/LocationRepo.java
@@ -1,9 +1,12 @@
 package com.edhealthbackend.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edhealthbackend.Model.Location;
 
 public interface LocationRepo extends JpaRepository<Location,Long>{
+    List<Location> findAllByType(String type);
 
 }
