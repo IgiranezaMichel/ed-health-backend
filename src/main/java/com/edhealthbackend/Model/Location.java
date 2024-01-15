84ff0f53e7ff6a84f0eb9cf8fc49926601e2cb21
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:36:33 +0200

    add location mapping

--- src/main/java/com/edhealthbackend/Model/Location.java
+++ src/main/java/com/edhealthbackend/Model/Location.java
@@ -1,4 +1,6 @@
 package com.edhealthbackend.Model;
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.*;
 @Entity
@@ -10,4 +12,18 @@ public class Location {
 private long id;
 private String name;
 private String type;
+@ManyToOne
+private Location location;
+@OneToMany(mappedBy = "location")
+private List<Location>locationList;
+public Location(long id,String name,String type,Location location){
+this.id=id;
+this.name=name;
+this.location=location;
+this.type=type;
+}
+@OneToMany(mappedBy = "location")
+private List<School> listOfSchool;
+@OneToMany(mappedBy = "location")
+private List<Hospital>listOfHospital;
 }
